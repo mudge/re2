a6b007fd1eebc6fd092877903f22dc690c300fb0
Stan Hu <stanhu@gmail.com>
2023-07-03 10:38:33 -0700

    Fix compilation with re2 2023-07-01
    https://code-review.googlesource.com/c/re2/+/61250 made re2 depend
    directly on abseil. The latest version of abseil now requires C++14
    (https://github.com/abseil/abseil-cpp/issues/1127). However, it seems
    that supplying `-std=c++14` isn't enough
    (https://github.com/abseil/abseil-cpp/issues/1431). `-std=c++17` needs
    to be used at least.
    
    This commit fixes the compilation by trying C++20 and C++17 if the
    initial compilation fails. This fixes build issues on macOS.

--- ext/re2/extconf.rb
+++ ext/re2/extconf.rb
@@ -44,24 +44,35 @@ unless have_library("re2")
   abort "You must have re2 installed and specified with --with-re2-dir, please see https://github.com/google/re2/wiki/Install"
 end
 
-# Recent versions of re2 now require a compiler with C++11 support
-checking_for("re2 requires C++11 compiler") do
+TRY_CXXFLAGS = %w[-std=c++20 -std=c++17 -std=c++11 -std=c++0x]
+
+# Recent versions of re2 depend directly on abseil, which requires a
+# compiler with C++14 support (see
+# https://github.com/abseil/abseil-cpp/issues/1127 and
+# https://github.com/abseil/abseil-cpp/issues/1431). However, the
+# `std=c++14` flag doesn't appear to suffice; we need at least
+# `std=c++17`.
+checking_for("re2 requires a C++14 compiler") do
+  success = false
   minimal_program = <<SRC
 #include <re2/re2.h>
 int main() { return 0; }
 SRC
 
-  unless try_compile(minimal_program, compile_options)
-    if try_compile(minimal_program, compile_options + " -std=c++11")
-      compile_options << " -std=c++11"
-      $CPPFLAGS << " -std=c++11"
-    elsif try_compile(minimal_program, compile_options + " -std=c++0x")
-      compile_options << " -std=c++0x"
-      $CPPFLAGS << " -std=c++0x"
-    else
-      abort "Cannot compile re2 with your compiler: recent versions require C++11 support."
+  if try_compile(minimal_program, compile_options)
+    success = true
+  else
+    TRY_CXXFLAGS.each do |version_flag|
+      if try_compile(minimal_program, compile_options + " #{version_flag}")
+        compile_options << " #{version_flag}"
+        $CPPFLAGS << " #{version_flag}"
+        success = true
+        break
+      end
     end
   end
+
+  abort "Cannot compile re2 with your compiler: recent versions require C++14 support." unless success
 end
 
 # Determine which version of re2 the user has installed.
