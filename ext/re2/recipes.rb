72cfdc2cd66b7f7685c1898a380e971ab3799dc7
Paul Mucur <mudge@mudge.name>
2025-06-28 08:22:03 +0100

    Upgrade Abseil to 20250512.1

--- ext/re2/recipes.rb
+++ ext/re2/recipes.rb
@@ -40,8 +40,6 @@ def build_recipe(name, version)
   MiniPortileCMake.new(name, version).tap do |recipe|
     recipe.target = File.join(PACKAGE_ROOT_DIR, 'ports')
     recipe.configure_options += [
-      # abseil needs a C++14 compiler
-      '-DCMAKE_CXX_STANDARD=14',
       # needed for building the C extension shared library with -fPIC
       '-DCMAKE_POSITION_INDEPENDENT_CODE=ON',
       # ensures pkg-config and installed libraries will be in lib, not lib64
