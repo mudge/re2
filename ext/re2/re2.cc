9260e438a00a1bdf4680f2392006f4b49561acd6
Paul Mucur <mudge@mudge.name>
2026-04-07 14:39:51 +0100

    Check startpos size on old RE2 ABI versions
    Older versions of RE2 only accept an integer startpos (rather than
    size_t) so check the value before casting and potentially truncating it
    when passing it to RE2's underlying Match function.

--- ext/re2/re2.cc
+++ ext/re2/re2.cc
@@ -1915,6 +1915,13 @@ static VALUE re2_regexp_match(int argc, VALUE *argv, const VALUE self) {
     rb_raise(rb_eArgError, "startpos should be <= endpos");
   }
 
+#ifndef HAVE_ENDPOS_ARGUMENT
+  /* Old RE2's Match() takes int startpos. Reject values that would overflow. */
+  if (startpos > INT_MAX) {
+    rb_raise(rb_eRangeError, "startpos should be <= %d", INT_MAX);
+  }
+#endif
+
   if (n == 0) {
     re2_match_args match_args;
     match_args.pattern = p->pattern;
